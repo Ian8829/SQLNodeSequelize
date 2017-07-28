88880969d45fe8fba4ec13bb19e1d02f0a641a1e
taeshik Hwang <ghjkghj@naver.com>
2017-07-28 14:42:56 +0900

    Creating Entries in a Table

--- models/index.js
+++ models/index.js
@@ -1,8 +1,9 @@
 'use strict';
 
-var fs        = require('fs');
-var path      = require('path');
-var Sequelize = require('sequelize');
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
+
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
